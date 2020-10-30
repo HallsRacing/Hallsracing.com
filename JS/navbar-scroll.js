9e0764b2689522719eb13aeb6e39bfe2dd08b67d
Zachary Varney <56404614+ZacharyVarney@users.noreply.github.com>
2020-10-30 16:49:02 -0400

    Navbar changes

--- JS/navbar-scroll.js
+++ JS/navbar-scroll.js
@@ -1,12 +1,15 @@
 const nav = document.querySelector('.navbar');
+const navButtons = document.querySelector('.nav-button-container');
 
 window.addEventListener("scroll", function(event){
     var scrolling = this.scrollY;
 
     if (scrolling > 0) {
-        nav.style.setProperty('box-shadow', '0px 3px 5px black');
+        nav.style.setProperty('box-shadow', '0px 2px 2px black');
+        navButtons.style.setProperty('box-shadow', '0px 2px 2px black');
     }
     else {
         nav.style.setProperty('box-shadow', '0px 0px 0px black');
+        navButtons.style.setProperty('box-shadow', '0px 0px 0px black');
     }
 });
